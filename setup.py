fb30843d2a94afec356046510170191e2bd4b56b
Filipp Lepalaan <filipp@mac.com>
2015-01-07 17:06:59 +0200

    PyPI version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gsxws",
-    version="0.53",
+    version="0.54",
     description="Library for communicating with GSX Web Services API.",
     long_description=open('README.rst').read(),
     install_requires=['PyYAML', 'lxml'],
@@ -18,7 +18,7 @@ setup(
     author="Filipp Lepalaan",
     author_email="filipp@fps.ee",
     url="https://github.com/filipp/py-gsxws",
-    download_url="https://github.com/filipp/py-gsxws/tarball/0.53",
+    download_url="https://github.com/filipp/py-gsxws/tarball/0.54",
     license="BSD",
     packages=find_packages(),
     package_data={'gsxws': ['products.yaml']}
