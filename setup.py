5f89dc1336e848efca8ece95e89a0ed7c151f68e
Filipp Lepalaan <filipp@mac.com>
2015-01-07 16:03:40 +0200

    PyPI fix for products.yaml

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="gsxws",
-    version="0.52",
+    version="0.53",
     description="Library for communicating with GSX Web Services API.",
     long_description=open('README.rst').read(),
     install_requires=['PyYAML', 'lxml'],
-    data_files=[('', 'products.yaml')],
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
@@ -19,7 +18,8 @@ setup(
     author="Filipp Lepalaan",
     author_email="filipp@fps.ee",
     url="https://github.com/filipp/py-gsxws",
-    download_url="https://github.com/filipp/py-gsxws/tarball/0.52",
+    download_url="https://github.com/filipp/py-gsxws/tarball/0.53",
     license="BSD",
     packages=find_packages(),
+    package_data={'gsxws': ['products.yaml']}
 )
